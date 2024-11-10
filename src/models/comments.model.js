d0542b35856a1bb8744dbc4e7e8eb772663eba8f
nikhilpatill18 <nikhilpatil6060@gmail.com>
2024-11-10 14:17:22 +0530

    created comment model

---
+++ src/models/comments.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from 'mongoose';
+
+const commentSchme = new Schema({
+
+    content: {
+        type: String,
+
+    },
+    video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Video'
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+
+}, { timestamps: true })
+
+export const Comments = mongoose.model('Comment', commentSchme);
