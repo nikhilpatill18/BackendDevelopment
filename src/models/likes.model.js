3243bc03f64fc9cf48ccdd5ab0e6593d52d59cce
nikhilpatill18 <nikhilpatil6060@gmail.com>
2024-11-10 14:33:44 +0530

    added liked model

---
+++ src/models/likes.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from 'mongoose';
+
+const likeSchema = new Schema({
+    comments: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Comment
+    },
+    video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Video'
+    },
+    likedby: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    tweets: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tweet'
+    }
+
+}, {
+    timestamps: true,
+})
+
+export const Like = mongoose.model('Like', likeSchema)
