b4a4163737c4df8f81990ea34592f4bf0c0b82fd
nikhilpatill18 <nikhilpatil6060@gmail.com>
2024-11-13 22:31:23 +0530

    "Added getPlaylistById and addVideoToPlaylist functionality to playlist controller and routes"

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,6 +3,7 @@ import { Playlist } from "../models/playlist.model.js"
 import { ApiError } from "../utils/apierror.js"
 import { Apiresponse } from "../utils/Apiresponse.js"
 import { asynchandler } from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
 
 
 const createPlaylist = asynchandler(async (req, res) => {
@@ -42,10 +43,48 @@ const getUserPlaylists = asynchandler(async (req, res) => {
 const getPlaylistById = asynchandler(async (req, res) => {
     const { playlistId } = req.params
     //TODO: get playlist by id
+
+    const getplaylistbyid = await Playlist.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(playlistId)
+            }
+        },
+    ])
+    if (!getplaylistbyid || getplaylistbyid.lengthbyid == 0) {
+        throw new ApiError(404, "Invlaid user id or user has no playlist")
+    }
+    return res.status(200).json(new Apiresponse(200, getplaylistbyid, "playlist found sucessfully"))
 })
 
 const addVideoToPlaylist = asynchandler(async (req, res) => {
     const { playlistId, videoId } = req.params
+    if (!playlistId) {
+        throw new ApiError(404, "You must specify a playlist")
+    }
+    if (!videoId) {
+        throw new ApiError(404, "You must specify video")
+    }
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(404, "Video id not found")
+    }
+    const playlist = await Playlist.findById(playlistId)
+    if (!playlist) {
+        throw new ApiError(404, "Playlist id not found")
+    }
+    const updated = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $addToSet: { videos: videoId }
+        },
+        { new: true }
+    )
+    if (!updated) {
+        throw new ApiError(404, "Faild to update playlist")
+    }
+    return res.status(200).json(new Apiresponse(200, updated, "video added to playlist"))
+
 })
 
 const removeVideoFromPlaylist = asynchandler(async (req, res) => {
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/Auth.middleware.js";
 import multer from "multer";
-import { createPlaylist, getUserPlaylists } from "../controllers/playlist.controller.js";
+import { createPlaylist, getUserPlaylists, getPlaylistById, addVideoToPlaylist } from "../controllers/playlist.controller.js";
 
 const playlistroute = Router();
 playlistroute.use(verifyJWT)
 
 playlistroute.route("/createplaylist").post(createPlaylist)
 playlistroute.route("/getuserplaylist/:userId").post(getUserPlaylists)
+playlistroute.route("/getuserplaylistbyid/:playlistId").post(getPlaylistById)
+playlistroute.route("/addvideotoplaylist/:playlistId/:videoId").post(addVideoToPlaylist)
 
 export default playlistroute;
